f697c863f7c0dd546a21cb6874e3a7cf02dfcccf
P-Rhyme <42650290+P-Rhyme@users.noreply.github.com>
2018-08-30 15:58:55 -0400

    Create Do_a_deer.rb

---
+++ Do_a_deer.rb
@@ -0,0 +1,53 @@
+# In-class work for Thursday, August 30
+
+use_bpm 120
+
+def metronome
+  live_loop :metro do
+    sample :elec_flip
+    sleep 1
+  end
+end
+
+def melody
+  live_loop :melody do
+    
+    use_synth :blade
+    
+    my_notes =     [:C3, :r, :D3, :E3, :r, :C3,
+                    :E3, :C3, :E3, :r]
+    
+    my_durations = [1, 0.5, 0.5, 1, 0.5, 0.5,
+                    1, 1, 1, 1]
+    
+    play_pattern_timed my_notes, my_durations
+  end
+end
+
+def harmony
+  live_loop :harmony do
+    
+    use_synth :piano
+    
+    my_notes =     [:E3, :r, :F3, :G3, :r, :E3,
+                    :G3]
+    
+    my_durations = [1, 0.5, 0.5, 1, 0.5, 0.5,
+                    4]
+    
+    play_pattern_timed my_notes, my_durations
+    
+  end
+end
+
+# Control panel:
+live_loop :control do
+  
+  metronome
+  melody
+  harmony
+  
+  # This sleep doesn't affect anything, but I
+  # need it so Sonic Pi doesn't whine:
+  sleep 1
+end
